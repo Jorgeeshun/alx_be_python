0ebba2bd60f1efc804583353a4b9c27311351377
jorgeeshun <cl>
2024-06-23 20:09:04 +0000

    Calculated the simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+# simple_interest.py
+
+# Define variable
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculate the simple interest
+interest = principal * rate * time
+
+# Print the calculated interest
+print("the simple is: {interest}" )
