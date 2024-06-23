0950f800a7f0178871150faba4358f1f14a3a397
jorgeeshun <cl>
2024-06-23 19:55:53 +0000

    Calculated sum, difference and product of two variables in 'basic_operations.py'

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,19 @@
+# basic_operations.py
+
+# Assigning the two variables with values
+number1 = 10
+number2 = 5
+
+# Calculate the sum
+sum = number1 + number2
+print ( "Addition of {number1} and  {number2} is {sum}" )
+
+# Calculate the difference
+diff = number1 - number2
+print ( "Subtraction of {number1} and  {number2} is {sum}" )
+
+# Calculate the product
+product = number1 * number2
+print ( "Multiplication of {number1} and  {number2} is {sum}" )
+
+
