8c78883e5a2097960208ddb5b9d760446295c28c
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2024-06-30 19:26:15 +0000

    new file

---
+++ weather_advice.py
@@ -0,0 +1,14 @@
+
+# Prompt the User for Weather Input
+weather = input("What's the weather like today? (Sunny/Rainy/Cold): ").strip().lower()
+
+
+# Provide Clothing Recommendations:
+if weather == 'sunny':
+     print("Wear a t-shirt and sunglasses.")
+elif weather == 'rainy':
+    print("Don't forget your umbrella and raincoat.")
+elif weather == 'cold':
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry I don't have a recomendations for this weather.")
