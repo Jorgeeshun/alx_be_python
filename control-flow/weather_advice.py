50e63e974b252da2b0edc20a6d475d15f30b4941
jorgeeshun <cl>
2024-06-30 18:19:45 +0000

    Prompt the user for weather input - 'if statement'

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,3 +1,14 @@
 # Prompt User for Weather Input:
-print("Whats the weather like toaday? (sunny/rainy/cold): ")
+user_input = print("Whats the weather like toaday? (Sunny/Rainy/Cold): ")
 
+# Prompt User for Weather Input:
+def cloth_recommendation():
+    if user_input.lower() == "sunny":
+        print("Wear a t-shirt and sunglasses.")
+    if user_input.lower():
+        print("Don't forget your umbrella and raincoat.")
+    if user_input.lower():
+        print("Make sure to wear a warm coat and a scarf.")
+
+    else:
+        print("Sorry I don't have a recomendations for this weather.")
