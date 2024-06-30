d826f9525963e8b5a698976e9a4b8169fd703e0c
jorgeeshun <cl>
2024-06-30 21:03:47 +0000

    Strings Formmatted

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -5,8 +5,5 @@ number = int(input("Enter a number to see its multiplication table: "))
 # Generate and Print the Multiplication Table:
 print(f"Multiplication Table for {number}: ")
 for i in range(1, 11):
-   X = number 
-   Y = i 
-   Z = X * Y
-   print(f"{X} * {Y} = {Z}")
-   
+    product = number * i
+    print(f"{number} * {i} = {product}")
