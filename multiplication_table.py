d051b67fb2b6e5c2c73ae666e4ca167699283016
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2024-06-30 20:52:43 +0000

    A python scripts to run a multiplication table

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,4 +1,12 @@
 # Prompt User for a Number:
 number = int(input("Enter a number to see its multiplication table: "))
 
+
 # Generate and Print the Multiplication Table:
+print(f"Multiplication Table for {number}: ")
+for i in range(1, 11):
+   X = number 
+   Y = i 
+   Z = X * Y
+   print(f"{X} * {Y} = {Z}")
+   
