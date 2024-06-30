7c10bc8d369010c7b7ea5e6a4f11bf2f005c8858
jorgeeshun <cl>
2024-06-30 20:23:48 +0000

    Added

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,29 @@
+try: 
+    # Prompt for User Input:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    operation_type = input("Choose the operation (+, -, *, /): ").strip()
+
+    # Perform the Calculation Using Match Case:
+    match operation_type:
+        case '+':
+            result = num1 + num2
+            print(f"The result is {result}.")
+        case '-':
+            result = num1 - num2
+            print(f"The result is {result}.")
+        case '*':
+            result = num1 * num2
+            print(f"The result is {result}.")
+        case '/':
+            if num2 != 0:
+                result = num1 / num2
+                print(f"The result is {result}.")
+            else:
+                print ("Error: Display by zero is not allowed.")
+        case _:
+            print("error: Invalid operation. Please choose from +, -, *, /.")
+
+except ValueError:
+    print("Error: Please enter valid numeric values.")
