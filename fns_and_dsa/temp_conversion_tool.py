c9681b926881f5b09c2edb63bc170e094747ff39
jorgeeshun <cl>
2024-07-07 22:39:24 +0000

    Changes to be committed: 	modified:   fns_and_dsa/temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 # Global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = "\n", 9 / 5
 
 def convert_to_celsius(fahrenheit):
     """
